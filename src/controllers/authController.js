3d7da4979d8eb6e34d8e03d7d68ad8158e7492fd
Parsa Ahmadi Nasab Emran <parsatempleowl@brandeis.edu>
2026-01-28 11:07:56 -0500

    fix: correct mongoose pre-save hook to properly use next

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -24,23 +24,19 @@ const registerUser = async (req, res) => {
 };
 
 // Login
-const loginUser = async (req, res, next) => {
-    try {
-        const { email, password } = req.body;
-        const user = await User.findOne({ email });
-        if (!user || !(await user.matchPassword(password))) {
-            return res.status(401).json({ message: "Invalid credentials" });
-        }
-
-        res.status(200).json({
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-            token: generateToken(user._id),
-        });
-    } catch (err) {
-        next(err);
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user || !(await user.matchPassword(password))) {
+        return res.status(401).json({ message: "Invalid credentials" });
     }
+
+    res.status(200).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id),
+    });
 };
 
 // Logout (frontend can just remove token, placeholder for future)
--- src/models/User.js
+++ src/models/User.js
@@ -27,9 +27,11 @@ const userSchema = new mongoose.Schema(
 
 // Hash password before saving
 userSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) return next();
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
+    if (!this.isModified("password")) {
+        return next();
+    }
+
+    this.password = await bcrypt.hash(this.password, 10);
     next();
 });
 
